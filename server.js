6bd8c5aee312e6046ef56680b6b54714c19481bc
Muhammad Athallah Zhuhry <muhammadzhuhry@gmail.com>
2021-12-07 09:04:09 +0700

    use env PORT

--- server.js
+++ server.js
@@ -48,6 +48,6 @@ app.get("/api/:date?", (req, res) => {
 
 
 // listen for requests :)
-var listener = app.listen(9000, function () {
+var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
